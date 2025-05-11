4f575fcc369055caf6a2dd06a84550bd72d4ac7c
thành việt <tv201306@gmail.com>
2025-05-11 17:17:32 +0700

    Add files via upload

--- quan_li_sinh_vien.cpp
+++ quan_li_sinh_vien.cpp
@@ -14,12 +14,41 @@ struct sinhvien{
     float diem,GPA;
     int somon;
 };
+struct thoikb{
+    string monhoc , phong;
+    int tiet;
+    int thu;
+};
+
+// ham tao lich hoc ngau nhien
+void lich(vector <thoikb>& tk_list){
+     vector <string> monhoc = {"giaitich" , "VLBD" ,"VL2","LTHDT","PBL","AnhVanB1"};
+     srand(time(0));
+     tk_list.clear();
+     for(int i = 0;i< monhoc.size();i++){
+        thoikb tk;
+        tk.monhoc = monhoc[i] ;
+        tk.phong = rand() % 100 + 1;
+        tk.thu = rand() % 8 + 2;
+        tk.tiet = rand() % 14 + 1;
+        tk_list.push_back(tk);
+     }
+     //hien thi thoi khoa bieu
+     cout <<"thoi khoa bieu cua sinh vien: " << endl;
+     for(auto &tk : tk_list){
+        cout << "mon hoc: " << tk.monhoc 
+             << "tiet: " << tk.tiet
+             << "thu: " << tk.thu 
+             << "phong: " << tk.phong << endl;
+     }
+}
 // ham them sinh vien
 void themsv(vector <sinhvien>& s){
      sinhvien sv;
-     cout << "nhap ma sinh vien: ";
-     getline(cin,sv.ID);
-     cout << "nhap ten sinh vien: ";
+     cout << "nhap ma sinh vien: " ;
+     getline(cin,sv.ID) ;
+     cin.ignore();
+     cout << "nhap ten sinh vien: " ;
      getline(cin,sv.name);
      cin.ignore();
      s.push_back(sv); 
@@ -39,6 +68,10 @@ void nhapdiem(vector <sinhvien>& s){
      for(auto &sv : s){
        cout << "nhap diem cho sinh vien: " << endl;
        cin >> sv.somon;
+       if(sv.somon <= 0){
+          cout << "so mon phai > 0" << endl;
+          return;
+       }
        float tong = 0;
        float diem_mon;
       for(int i = 0;i<sv.somon;i++){
@@ -48,13 +81,13 @@ void nhapdiem(vector <sinhvien>& s){
       }
       sv.diem = tong;
       sv.GPA = sv.diem / sv.somon ;
-      cout << endl;
+      cout << "GPA cua sinh vien la: " << sv.GPA << endl;
      }
 }
 // ham luu danh sach vao file
 void savestudents(vector <sinhvien>& sv, const string& filename){
    fstream sfile;
-   sfile.open("luu_file.txt",ios::out|ios::in|ios::app);
+   sfile.open("luu_file.txt",ios::out|ios::app);
    for(auto &sv_item : sv){
       sfile << sv_item.name << "," << sv_item.ID << "," << sv_item.GPA << endl;
    }
@@ -63,7 +96,8 @@ void savestudents(vector <sinhvien>& sv, const string& filename){
 }
 // doc danh sach tu file
 void readstudents(vector<sinhvien>& sv, const string& filename){
-   fstream infile(filename, ios::in);
+   fstream infile;
+   infile.open("danh_sach.txt" , ios::in);
    if(!infile){
        cout << "khong mo duoc file!" << endl;
        return;
@@ -106,7 +140,6 @@ void arrange(vector <sinhvien>& sv_list){
 }
 // tim sinh vien 
 void lookfor(const vector <sinhvien>& sv ){
-    vector <char> N; 
     string Name;
     cout << "nhap ten can tim: " ;
     cin.ignore();
@@ -122,47 +155,42 @@ void lookfor(const vector <sinhvien>& sv ){
       cout <<"khong tim thay sinh vien!"<< endl;
     }
 }
-
-int main(){
-    vector <sinhvien> s;
+  int main() {
+    vector<sinhvien> s;
     int choice;
-    cin >> choice;
-    while(true){
-       cout << "lua chon: " << endl;
-       cout << "0. thoat chuong trinh " << endl;
-       cout << "1. them sinh vien " << endl;
-       cout << "2. hien thi danh sach sinh vien " << endl;
-       cout << "3. xep loai sinh vien " << endl;
-       cout << "4. luu danh sach sinh vien vao file " << endl;
-       cout << "5. doc danh sach tu file " << endl;
-       switch(choice){
-        case 1:
-          // them sinh vien
-          themsv(s) ;
-          break;
-          case 2:
-          // hien thi danh sach sinh vien
-          show(s);
-          break;
-          case 3:
-          // xep loai hoc luc
-          arrange(s);
-          break;
-          case 4:
-          // luu danh sach vao file
-          savestudents(s,"luu_file.txt");
-          break;
-          case 5:
-          // doc danh sach tu file
-          readstudents(s,"danh_sach.txt");
-          break;
-          case 0:
-          cout << "thoat chuong trinh: " << endl;
-          break;
-          default:
-          cout << "thoat chuong trinh !" << endl;
-     }
-   }
+    do {
+        cout << "lua chon: " << endl;
+        cout << "0. thoat chuong trinh " << endl;
+        cout << "1. them sinh vien " << endl;
+        cout << "2. hien thi danh sach sinh vien " << endl;
+        cout << "3. xep loai sinh vien " << endl;
+        cout << "4. luu danh sach sinh vien vao file " << endl;
+        cout << "5. doc danh sach tu file " << endl;
+        cout << "Nhap lua chon: ";
+        cin >> choice;     
+        cin.ignore(); 
+        switch (choice) {
+            case 1:
+                themsv(s);
+                break;
+            case 2:
+                show(s);
+                break;
+            case 3:
+                arrange(s);
+                break;
+            case 4:
+                savestudents(s, "luu_file.txt");
+                break;
+            case 5:
+                readstudents(s, "danh_sach.txt");
+                break;
+            case 0:
+                cout << "thoat chuong trinh!" << endl;
+                break;
+            default:
+                cout << "lua chon khong hop le!" << endl;
+        }
+    } while (choice != 0);
+    return 0;
 }
-
-
